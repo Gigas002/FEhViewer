073ca27fa5eaf5515540f15db89aca52d174004d
gigas002 <gigas002@pm.me>
2022-08-30 11:53:12 +0900

    WIP on apk publish in ci

--- scripts/build_apk.sh
+++ scripts/build_apk.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 source para.sh
-apk_build_path="$scripts_path/../build/app/outputs/apk/release/";
-apk_build_path_universal="$scripts_path/../build/app/outputs/apk/releaseUniversal/";
+apk_build_path="$scripts_path/../build/app/outputs/flutter-apk/";
+apk_build_path_universal="$scripts_path/../build/app/outputs/flutter-apk/releaseUniversal/";
 
-#flutter build apk --target-platform android-arm,android-arm64,android-x64 --split-per-abi
 flutter build apk
 cp -R $apk_build_path $apk_build_path_universal
 flutter build apk --split-per-abi

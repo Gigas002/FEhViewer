981b1cd978d25a810a0bf780e6c1274ec65cf212
Gigas002 <24297712+Gigas002@users.noreply.github.com>
2022-08-30 08:47:23 +0000

    Update build_apk.sh

--- scripts/build_apk.sh
+++ scripts/build_apk.sh
@@ -9,6 +9,25 @@ apk_build_tmp="$scripts_path/../build/app/outputs/tmp/";
 
 flutter build apk
 mkdir $apk_build_tmp
+
+echo "Test1"
+ls -lR $apk_build_path
+ls -lR $apk_build_tmp
+
 cp -R $apk_build_path $apk_build_tmp
+
+echo "Test2"
+ls -lR $apk_build_path
+ls -lR $apk_build_tmp
+
 flutter build apk --split-per-abi
+
+echo "Test3"
+ls -lR $apk_build_path
+ls -lR $apk_build_tmp
+
 cp -i $apk_build_tmp/* $apk_build_path/*
+
+echo "Test4"
+ls -lR $apk_build_path
+ls -lR $apk_build_tmp

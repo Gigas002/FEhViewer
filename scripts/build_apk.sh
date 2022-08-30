b64e1caca8cc2fe79f92290fedad28f9033e36fd
Gigas002 <24297712+Gigas002@users.noreply.github.com>
2022-08-30 03:44:21 +0000

    Update build_apk.sh

--- scripts/build_apk.sh
+++ scripts/build_apk.sh
@@ -1,13 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-ls -a
 source para.sh
-echo $scripts_path
 
-#apk_build_path="$scripts_path/../build/app/outputs/flutter-apk/";
-#apk_build_path_universal="$scripts_path/../build/app/outputs/flutter-apk/releaseUniversal/";
+apk_build_path="$scripts_path/../build/app/outputs/flutter-apk/";
+apk_build_path_universal="$scripts_path/../build/app/outputs/flutter-apk/releaseUniversal/";
 
-#flutter build apk
-#cp -R $apk_build_path $apk_build_path_universal
-#flutter build apk --split-per-abi
-#cp $apk_build_path_universal/*.apk $apk_build_path/*
+flutter build apk
+cp -R $apk_build_path $apk_build_path_universal
+flutter build apk --split-per-abi
+cp $apk_build_path_universal/*.apk $apk_build_path/*
